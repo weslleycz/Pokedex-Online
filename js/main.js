cf249f8fbedb6d026d5f95eccc706aa7dc40ce2c
Weslley <weslleysouza22@gmail.com>
2022-02-17 18:32:03 -0300

    feat: Sistema de seleção de Pokémon que mostra as informações em modal.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const url = 'https://pokeapi.co/api/v2/pokemon/';
+let pokemons = [];
 const fetchPoke = async () => {
-    let pokemons = [];
     for (i = 1; i <= 151; i++) {
         const dados = await fetch(`${url}${i}`);
         const pokemon = await dados.json();
@@ -22,10 +22,12 @@ function mostraPokemonsHtml(pokemons) {
             ${pokemon.types[1].type.name}</div>`
         } else(type = `<div class="${pokemon.types[0].type.name}">
         ${pokemon.types[0].type.name}</div>`);
-
+    
         return `<div class="col-sm"><div class="dex-list">
-            <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <div class="iten-dex"><img class="dex-img"
+            <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal"
+            onclick="selectPokemon(${pokemon.id-1})">
+            <div class="iten-dex">
+            <img class="dex-img"
             src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg">
             <div class="id-dex">N°${pokemon.id}</div>
             <h3>${pokemon.name[0].toUpperCase()+pokemon.name.substr(1)}</h3>
@@ -37,7 +39,29 @@ function mostraPokemonsHtml(pokemons) {
     for (let index = 0; index < cards.length; index++) {
             pokemonList = pokemonList + cards[index]
     }
-    html.innerHTML = row + pokemonList + div
+    html.innerHTML = row + pokemonList + div;
+}
+
+function selectPokemon(n){
+    if (pokemons[n]!=undefined) {
+        let html = document.getElementById('modal-body');
+        html.innerHTML = `<b class="pokemon-nome"> ${pokemons[n]
+            .name[0].toUpperCase()+pokemons[n].name.substr(1)}</b>
+        <div class="modal-dialog">
+           <div class="pokemon-card">
+               <img class="pokemon-img" src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${n+1}.svg"">
+        </div>
+        </div>
+        <div class="card-down">
+        <div class="btns-container">
+        </div>
+        <h3>Altura: ${pokemons[n].height/10}m </h3>
+        <h3>Peso: ${pokemons[n].weight/10}kg</h3>
+        <h3>Status Base: ${pokemons[n].stats[0].base_stat}</h3>
+        <h3>Experiencia base: ${pokemons[n].base_experience}</h3>
+        </div>
+        `;
+    }
 }
 
 fetchPoke();
